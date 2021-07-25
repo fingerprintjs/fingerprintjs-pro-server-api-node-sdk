3ae8c18aaf28768ae70d390b48c52570a363281d
Martin Makarský <martinmakarsky@gmail.com>
2021-07-25 13:48:47 +0200

    feat: add webhook visitor type

--- src/types.ts
+++ src/types.ts
@@ -68,3 +68,94 @@ export interface BrowserDetails {
   device: string;
   userAgents: string;
 }
+
+export interface WebookVisitor {
+    // Unique request identifier
+    // nullable: false, maxLength: 20
+    requestId: string;
+    // customer-provided data, for example requestType and yourCustomId
+    // both the tag and the information it contains are optional
+    // and only for the customer's need.
+    // nullable: true, maxLength: 4096
+    tag: any | null,
+    // user-provided scalar identifier
+    // nullable: true, maxLength: 4096
+    linkedId: string | null;
+    // persistent visitor identifier
+    // helpful to detect anonymous or private mode users
+    // nullable: false, maxLength: 20
+    visitorId: string;
+    // timestamp in milliseconds (since unix epoch)
+    // nullable: false
+    timestamp: number;
+    // if the page view was made in incognito or private mode
+    // nullable: false
+    incognito: boolean;
+    // URL where the API was called in the browser
+    // nullable: false, maxLength: 4096
+    url: string;
+    // The URL of a client-side referrer
+    // nullable: true, maxLength: 4096
+    clientReferrer: string | null;
+    // nullable: false, maxLength: 40
+    ip: string;
+    // nullable: false
+    ipLocation: {
+      // miles
+      // nullable: true
+      accuracyRadius: number | null;
+      // nullable: true
+      city: {
+        // nullable: true, maxLength: 4096
+        name: string | null;
+      } | null,
+      // nullable: true
+      continent: {
+        // nullable: true, maxLength: 2
+        code: string | null;
+        // nullable: true, maxLength: 40
+        name: string | null;
+      } | null,
+      // nullable: true
+      country: {
+        // nullable: true, maxLength: 2
+        code: string | null;
+        // nullable: true, maxLength: 250
+        name: string | null;
+      } | null,
+      // nullable: true
+      latitude: number | null;
+      // nullable: true
+      longitude: number | null;
+      // nullable: true, maxLength: 40
+      postalCode: string | null;
+      // nullable: true
+      subdivisions: [
+        {
+          // nullable: true, maxLength: 250
+          isoCode: string | null;
+          // nullable: true, maxLength: 250
+          name: string | null;
+        }
+      ] | null;
+      // nullable: true, maxLength: 250
+      timezone: string | null;
+    },
+    // nullable: false
+    browserDetails: {
+      // nullable: true, maxLength: 250
+      browserName: string | null;
+      // nullable: true, maxLength: 250
+      browserFullVersion: string | null;
+      // nullable: true, maxLength: 250
+      browserMajorVersion: string | null,
+      // nullable: true, maxLength: 250
+      os: string | null,
+      // nullable: true, maxLength: 250
+      osVersion: string | null,
+      // nullable: true, maxLength: 250
+      device: string | null,
+      // nullable: true, maxLength: 4096
+      userAgent: string | null,
+    }
+}
---
+++ tests/mocked-responses-tests/castVisitorWebookTest.spec.ts
@@ -0,0 +1,31 @@
+import { WebookVisitor } from './../../src/types';
+import visitorWebhookBody from './mocked-responses-data/visitor-webhook-body.json'
+
+describe('[Mocked body] Cast visitor webhook', () => {
+
+  test('with sample request body', async () => {
+    const requestBody = visitorWebhookBody as unknown as WebookVisitor;
+
+    expect(requestBody).toMatchSnapshot();
+
+    // TODO: check all properties manually
+    // expect(requestBody.requestId).toEqual("1627054081281.wo4lLc");
+    // expect(requestBody.visitorId).toEqual("TaDnMBz9XCpZNuSzFUqP");
+
+    // expect(requestBody.browserDetails.browserName).toEqual("Chrome");
+    // expect(requestBody.browserDetails.browserMajorVersion).toEqual("92");
+    // expect(requestBody.browserDetails.browserFullVersion).toEqual("92.0.4515");
+    // expect(requestBody.browserDetails.os).toEqual("Windows");
+    // expect(requestBody.browserDetails.osVersion).toEqual("10");
+    // expect(requestBody.browserDetails.device).toEqual("Other");
+    // expect(requestBody.browserDetails.userAgent).toEqual("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36");  
+    // // not documented
+    // // expect(requestBody.browserDetails.botProbability).toEqual(0);
+
+    // expect(requestBody.incognito).toEqual(false);
+    // expect(requestBody.ip).toEqual("185.5.70.59");
+
+    // expect(requestBody.ipLocation.accuracyRadius).toEqual(100);
+    // expect(requestBody.ipLocation.city).toEqual(null);
+  });
+});
