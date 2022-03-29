97d8c39aa71b36cd302581cbc450d1f9f35e1ca5
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2022-03-29 14:12:18 +0200

    feat: add confidence score, visitor found, and timestamps to data types

--- src/types.ts
+++ src/types.ts
@@ -28,6 +28,9 @@ export interface Options {
   authenticationMode?: AuthenticationMode;
 }
 
+/**
+ * More info: https://dev.fingerprintjs.com/docs/server-api#query-parameters
+ */
 export type VisitorHistoryFilter = {
   request_id?: string;
   linked_id?: string;
@@ -35,22 +38,29 @@ export type VisitorHistoryFilter = {
   before?: number;
 };
 
+/**
+ * More info: https://dev.fingerprintjs.com/docs/server-api#response
+ */
 export interface VisitorsResponse {
-  lastTimestamp?: string;
   visitorId: string;
   visits: Visit[];
+  lastTimestamp?: number;
 }
 
 export interface Visit {
   requestId: string;
   incognito: string;
-  linkedId: 'string';
+  linkedId: string;
   time: Date;
   timestamp: number;
   url: string;
   ip: string;
   ipLocation: IpLocation;
   browserDetails: BrowserDetails;
+  confidence: Confidence;
+  visitorFound: boolean;
+  firstSeenAt: SeenAt;
+  lastSeenAt: SeenAt;
 }
 
 export interface IpLocation {
@@ -94,93 +104,64 @@ export interface BrowserDetails {
   userAgents: string;
 }
 
+export interface Confidence {
+  score: number;
+}
+
+export interface SeenAt {
+  global: Date | null;
+  subscription: Date | null;
+}
+
+/**
+ * More info: https://dev.fingerprintjs.com/docs/webhooks#identification-webhook-object-format
+ */
 export interface VisitWebhook {
-  // Unique request identifier
-  // nullable: false, maxLength: 20
   requestId: string;
-  // customer-provided data, for example requestType and yourCustomId
-  // both the tag and the information it contains are optional
-  // and only for the customer's need.
-  // nullable: true, maxLength: 4096
   tag: any | null;
-  // user-provided scalar identifier
-  // nullable: true, maxLength: 4096
   linkedId: string | null;
-  // persistent visitor identifier
-  // helpful to detect anonymous or private mode users
-  // nullable: false, maxLength: 20
   visitorId: string;
-  // timestamp in milliseconds (since unix epoch)
-  // nullable: false
+  visitorFound: boolean | null;
   timestamp: number;
-  // if the page view was made in incognito or private mode
-  // nullable: false
+  time: Date;
   incognito: boolean;
-  // URL where the API was called in the browser
-  // nullable: false, maxLength: 4096
   url: string;
-  // The URL of a client-side referrer
-  // nullable: true, maxLength: 4096
   clientReferrer: string | null;
-  // nullable: false, maxLength: 40
   ip: string;
-  // nullable: false
   ipLocation: {
-    // miles
-    // nullable: true
     accuracyRadius: number | null;
-    // nullable: true
     city: {
-      // nullable: true, maxLength: 4096
       name: string | null;
     } | null;
-    // nullable: true
     continent: {
-      // nullable: true, maxLength: 2
       code: string | null;
-      // nullable: true, maxLength: 40
       name: string | null;
     } | null;
-    // nullable: true
     country: {
-      // nullable: true, maxLength: 2
       code: string | null;
-      // nullable: true, maxLength: 250
       name: string | null;
     } | null;
-    // nullable: true
     latitude: number | null;
-    // nullable: true
     longitude: number | null;
-    // nullable: true, maxLength: 40
     postalCode: string | null;
-    // nullable: true
     subdivisions:
       | {
-          // nullable: true, maxLength: 250
           isoCode: string | null;
-          // nullable: true, maxLength: 250
           name: string | null;
         }[]
       | null;
-    // nullable: true, maxLength: 250
     timezone: string | null;
   };
-  // nullable: false
   browserDetails: {
-    // nullable: true, maxLength: 250
     browserName: string | null;
-    // nullable: true, maxLength: 250
     browserFullVersion: string | null;
-    // nullable: true, maxLength: 250
     browserMajorVersion: string | null;
-    // nullable: true, maxLength: 250
     os: string | null;
-    // nullable: true, maxLength: 250
     osVersion: string | null;
-    // nullable: true, maxLength: 250
     device: string | null;
-    // nullable: true, maxLength: 4096
     userAgent: string | null;
   };
+  confidence: Confidence | null;
+  firstSeenAt: SeenAt;
+  lastSeenAt: SeenAt;
 }
