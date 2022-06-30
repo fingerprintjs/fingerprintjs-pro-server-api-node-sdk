3136d5ace2ad23558ff9dfccc4297a984982cd9c
Ilya Taratukhin <darkilfa@gmail.com>
2022-06-30 18:35:27 +0300

    feat: generate types using OpenAPI

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 import type fetchFn from 'node-fetch';
+import { components, paths } from './generatedApiTypes';
 
 export enum Region {
   EU = 'EU',
@@ -38,137 +39,15 @@ export interface Options {
 /**
  * More info: https://dev.fingerprintjs.com/docs/server-api#query-parameters
  */
-export type VisitorHistoryFilter = {
-  request_id?: string;
-  linked_id?: string;
-  limit?: number;
-  before?: number;
-};
+export type VisitorHistoryFilter = paths['/visitors/{visitor_id}']['get']['parameters']['query'];
 
 /**
  * More info: https://dev.fingerprintjs.com/docs/server-api#response
  */
-export interface VisitorsResponse {
-  visitorId: string;
-  visits: Visit[];
-  lastTimestamp?: number;
-}
-
-export interface Visit {
-  requestId: string;
-  incognito: string;
-  linkedId: string;
-  time: Date;
-  timestamp: number;
-  url: string;
-  ip: string;
-  ipLocation: IpLocation;
-  browserDetails: BrowserDetails;
-  confidence: Confidence;
-  visitorFound: boolean;
-  firstSeenAt: SeenAt;
-  lastSeenAt: SeenAt;
-}
-
-export interface IpLocation {
-  accuracyRadius: number;
-  latitude: number;
-  longitude: number;
-  postalCode: string;
-  timezone: string;
-  city: City;
-  continent: Continent;
-  country: Country;
-  subdivisions: Subdivision[];
-}
-
-export interface City {
-  name: string;
-}
-
-export interface Continent {
-  code: string;
-  name: string;
-}
-
-export interface Country {
-  code: string;
-  name: string;
-}
-
-export interface Subdivision {
-  isoCode: string;
-  name: string;
-}
-
-export interface BrowserDetails {
-  browserName: string;
-  browserMajorVersion: string;
-  browserFullVersion: string;
-  os: string;
-  osVersion: string;
-  device: string;
-  userAgents: string;
-}
-
-export interface Confidence {
-  score: number;
-}
-
-export interface SeenAt {
-  global: Date | null;
-  subscription: Date | null;
-}
+export type VisitorsResponse =
+  paths['/visitors/{visitor_id}']['get']['responses']['200']['content']['application/json'];
 
 /**
  * More info: https://dev.fingerprintjs.com/docs/webhooks#identification-webhook-object-format
  */
-export interface VisitWebhook {
-  requestId: string;
-  tag: any | null;
-  linkedId: string | null;
-  visitorId: string;
-  visitorFound: boolean | null;
-  timestamp: number;
-  time: Date;
-  incognito: boolean;
-  url: string;
-  clientReferrer: string | null;
-  ip: string;
-  ipLocation: {
-    accuracyRadius: number | null;
-    city: {
-      name: string | null;
-    } | null;
-    continent: {
-      code: string | null;
-      name: string | null;
-    } | null;
-    country: {
-      code: string | null;
-      name: string | null;
-    } | null;
-    latitude: number | null;
-    longitude: number | null;
-    postalCode: string | null;
-    subdivisions:
-      | {
-          isoCode: string | null;
-          name: string | null;
-        }[]
-      | null;
-    timezone: string | null;
-  };
-  browserDetails: {
-    browserName: string | null;
-    browserFullVersion: string | null;
-    browserMajorVersion: string | null;
-    os: string | null;
-    osVersion: string | null;
-    device: string | null;
-    userAgent: string | null;
-  };
-  confidence: Confidence | null;
-  firstSeenAt: SeenAt;
-  lastSeenAt: SeenAt;
-}
+export type VisitWebhook = components['schemas']['Visit'];
