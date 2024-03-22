8b784beb61e929e88e15efba148de56d46e5e34a
Orkun <orkuncakilkaya@gmail.com>
2024-03-22 14:31:32 +0300

    chore: remove redundant mock files

--- sync.sh
+++ sync.sh
@@ -3,19 +3,12 @@
 curl -o ./resources/fingerprint-server-api.yaml https://fingerprintjs.github.io/fingerprint-pro-server-api-openapi/schemas/fingerprint-server-api-compact.yaml
 
 examplesList=(
-  'get_visits_200_limit_1.json'
-  'get_visits_200_limit_500.json'
   'webhook.json'
   'get_event_200.json'
   'get_event_200_all_errors.json'
   'get_event_200_extra_fields.json'
   'get_event_403_error.json'
   'get_event_404_error.json'
-  'get_event_200_botd_failed_error.json'
-  'get_event_200_botd_too_many_requests_error.json'
-  'get_event_200_identification_failed_error.json'
-  'get_event_200_identification_too_many_requests_error.json'
-  'get_visits_429_too_many_requests_error.json'
 )
 
 for example in ${examplesList[*]}; do
