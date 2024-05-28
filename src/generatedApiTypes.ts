a664c2ddb051d1bc0f3ef3fb19a1116933cdbd16
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-05-28 16:53:08 +0900

    feat: add revision string field to confidence object

--- src/generatedApiTypes.ts
+++ src/generatedApiTypes.ts
@@ -371,6 +371,8 @@ export interface components {
        * @description The confidence score is a floating-point number between 0 and 1 that represents the probability of accurate identification.
        */
       score: number
+      /** @description The revision name of the method used to calculate the Confidence score. This field is only present for customers who opted in to an alternative calculation method. */
+      revision?: string
     }
     /**
      * SeenAt
