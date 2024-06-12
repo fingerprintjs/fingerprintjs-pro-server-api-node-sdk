1a2a21a3b74250e00c9336ece9ae6a11b2d96e53
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-06-12 10:12:23 +0200

    refactor: rename webhook signature functions

--- src/webhook.ts
+++ src/webhook.ts
@@ -1,10 +1,10 @@
 import crypto from 'crypto'
 
-function checkWebhookSignature(signature: string, data: Buffer, secret: string) {
+function isValidHmacSignature(signature: string, data: Buffer, secret: string) {
   return signature === crypto.createHmac('sha256', secret).update(data).digest('hex')
 }
 
-export interface IsValidHmacSignatureParams {
+export interface IsValidWebhookSignatureParams {
   /**
    * The value of the "fpjs-event-signature" header.
    * */
@@ -23,7 +23,7 @@ export interface IsValidHmacSignatureParams {
  * Verifies the HMAC signature extracted from the "fpjs-event-signature" header of the incoming request. This is a part of the webhook signing process, which is available only for enterprise customers.
  * If you wish to enable it, please contact our support: https://fingerprint.com/support
  *
- * @param {IsValidHmacSignatureParams} params
+ * @param {IsValidWebhookSignatureParams} params
  * @param {string} params.signatureHeader - The value of the "fpjs-event-signature" header.
  * @param {Buffer} params.data - The raw data of the incoming request.
  * @param {string} params.secret - The secret key used to sign the request.
@@ -53,14 +53,14 @@ export interface IsValidHmacSignatureParams {
  * }
  * ```
  */
-export function isValidHmacSignature(params: IsValidHmacSignatureParams): boolean {
+export function isValidWebhookSignature(params: IsValidWebhookSignatureParams): boolean {
   const { header, data, secret } = params
 
   const signatures = header.split(',')
   for (const signature of signatures) {
     const [version, hash] = signature.split('=')
     if (version === 'v1') {
-      if (checkWebhookSignature(hash, data, secret)) {
+      if (isValidHmacSignature(hash, data, secret)) {
         return true
       }
     }
